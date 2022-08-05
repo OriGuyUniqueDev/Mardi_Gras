3a37c0ed921329e47b744220eaabb55664e12d43
OriGuyUniqueDev <origuydev@gmail.com>
2022-08-05 16:34:07 +0300

    final project

--- javaScript/app.js
+++ javaScript/app.js
@@ -2,6 +2,7 @@ let fname = document.querySelector(".fname");
 let lname = document.querySelector(".lname");
 let email = document.querySelector(".email");
 let btn = document.querySelector(".sendBTN");
+let message = document.querySelector(".message");
 let formInputs = [fname, lname, email];
 const regex = /\d/;
 
@@ -13,8 +14,16 @@ function validationInputs() {
 	let fnameStr = fname.value;
 	let lnameStr = lname.value;
 	let emailStr = email.value;
+	let messageStr = message.value;
+	let passTest = 0
+	let info = {
+		name: `${fnameStr} ${lnameStr}`,
+		email: emailStr,
+		userMessage : messageStr
+	}
 	if (!regex.test(fnameStr) && fnameStr.length >= 2) {
 		fname.style.border = "5px solid green";
+		passTest++
 	} else {
 		alert("❌ First Name should contain only letters and should be at least 2 characters ❌");
 		fname.style.border = "5px solid red";
@@ -22,6 +31,7 @@ function validationInputs() {
 
 	if (!regex.test(lnameStr) && lnameStr.length >= 2) {
 		lname.style.border = "5px solid green";
+		passTest++
 	} else {
         alert("❌ Last Name should contain only letters and should be at least 2 characters ❌");
 		lname.style.border = "5px solid red";
@@ -29,8 +39,13 @@ function validationInputs() {
 
 	if (emailStr.includes("@") && emailStr.includes(".")) {
 		email.style.border = "5px solid green";
+		passTest++
 	} else {
         alert("❌ The Email must include one \"@\" and at least one \".\" ❌");
 		email.style.border = "5px solid red";
+	}
+	if(passTest === 3){
+		alert("✅Your Message as been sent, We Will Be in Touch Soon...✅")
+		console.log(info)
 	}
 }
